a26099c2c9365dac0623b126251662bc9efb6789
Mohammedelhansaly <elhansalymohammed@gmail.com>
2026-01-01 15:16:22 +0100

    handle unknown unit type in ft_seed_inventory function

--- ex7/ft_seed_inventory.py
+++ ex7/ft_seed_inventory.py
@@ -6,4 +6,4 @@ def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
     elif unit == "area":
         print(f"{seed_type.title()} seeds: covers {quantity} square meters")
     else:
-        print(f"{seed_type.title()} seeds: covers {quantity} {unit}")
+        print(f"Unknown unit type")
